5be8baad3719b33b01de69360da967739befa754
Christian Reed <reed@Christians-MacBook-Pro.local>
2025-11-17 12:00:38 +0100

    test api

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
+import PetDetails from './pages/PetDetails.jsx';
+import { BrowserRouter, Routes, Route } from 'react-router';
 
 createRoot(document.getElementById('root')).render(
-	<StrictMode>
-		<App />
-	</StrictMode>
+	<BrowserRouter>
+		<Routes>
+			<Route path="/" element={<App />} />
+			<Route path="/pet/:id" element={<PetDetails />} />
+		</Routes>
+	</BrowserRouter>
 );
